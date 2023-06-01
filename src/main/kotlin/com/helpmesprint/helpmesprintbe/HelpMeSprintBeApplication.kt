861c167fd5205724bc5d27f9996ed28a25eb3548
Avnish Desai <avnish.desai@gmail.com>
2023-06-01 21:15:33 +0200

    initial commit

---
+++ src/main/kotlin/com/helpmesprint/helpmesprintbe/HelpMeSprintBeApplication.kt
@@ -0,0 +1,11 @@
+package com.helpmesprint.helpmesprintbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelpMeSprintBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelpMeSprintBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/helpmesprint/helpmesprintbe/HelpMeSprintBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.helpmesprint.helpmesprintbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelpMeSprintBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
